5c07a74278c48194b5e0724e7356513b98e5b68b
Rohit kumar <rohit_designer@outlook.com>
2023-08-25 12:55:14 +0200

    added router route and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import Navbar from "./components/header/Navbar";
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import Layout from "./Layout/index";
+import { Route, Routes } from "react-router-dom";
+
+const Homepage = lazy(() => import("./pages/home/HomePage"));
+const DetailPage = lazy(() => import("./pages/specific/DetailPage"));
+const SignIn = lazy(() => import("./pages/signIn/SignIn"));
+const SignUp = lazy(() => import("./pages/signUp/SignUp"));
+const Venues = lazy(() => import("./pages/venues/Venues"));
+const UserProfile = lazy(() => import("./pages/userProfile/Profile"));
+const PageNotFound = lazy(() => import("./pages/pageNotfound/ErrorPage"));
+
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/venues" element={<Venues />} />
+          <Route path="/venues/:id" element={<DetailPage />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/error" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
