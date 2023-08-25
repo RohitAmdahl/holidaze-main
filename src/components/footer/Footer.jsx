31cd4c6bf8a9c91d78a5c373d06b0ff1148f2ff6
Rohit kumar <rohit_designer@outlook.com>
2023-08-25 14:48:51 +0200

    footer design almost done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import logo from "../../assets/logo.png";
+import footerLogo from "../../assets/footer_logo.png";
+import { BsFacebook } from "react-icons/bs";
+import { ImInstagram } from "react-icons/im";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" flex justify-between items-center container mx-auto max-w-4xl">
+      <div className="">
+        <img src={logo} alt="Logo" className="w-1/2" />
+      </div>
+      <ul className="flex justify-center items-center">
+        <li className="px-2">
+          <BsFacebook size={20} className=" text-sky-700" />
+        </li>
+        <li className="px-2">
+          <ImInstagram size={20} className="text-red-700" />
+        </li>
+      </ul>
+      <div className="flex flex-col  ">
+        <span className="">© Holidaze {new Date().getFullYear()},</span>
+        <span className="">
+          Designed and coding by
+          <img
+            src={footerLogo}
+            alt="footer logo designer and coder "
+            className=""
+          />
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
