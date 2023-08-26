67a7b66bebce6b07c2120d0ae2266ea905ca226f
Rohit kumar <rohit_designer@outlook.com>
2023-08-27 01:51:46 +0200

    font edit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../header/Navbar";
 const Header = () => {
   return (
-    <header className=" container mx-auto max-w-4xl  ">
+    <header className=" container mx-auto max-w-4xl font-Montserrat">
       <Navbar />
     </header>
   );
