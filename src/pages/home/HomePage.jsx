2d3e7c96bd61a96570178a0739f356993561fce6
Rohit kumar <rohit_designer@outlook.com>
2023-08-27 01:52:27 +0200

    added location component

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroImg from "../../components/home/HeroImg";
+import Location from "../../components/home/Location";
 
 const HomePage = () => {
   return (
     <>
       <HeroImg />
+      <Location />
     </>
   );
 };
