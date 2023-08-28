8847d29f867b7b089c5034721786eb9c228673ce
Rohit kumar <rohit_designer@outlook.com>
2023-08-29 01:01:07 +0200

    data fetch

--- src/hooks/DataFetch.jsx
+++ src/hooks/DataFetch.jsx
@@ -6,7 +6,7 @@ import Card from "../components/cards/Card";
 // console.log(BASE_URL);
 const DataFetch = () => {
   const [venues, setVenues] = useState([]);
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
@@ -28,7 +28,7 @@ const DataFetch = () => {
     getData();
   }, []);
 
-  if (isLoading) {
+  if (isLoading && !venues.length) {
     return (
       <div className=" flex justify-center items-center mt-3 mb-3 ">
         <ClimbingBoxLoader size={15} color="#6E7A55" />
@@ -36,13 +36,17 @@ const DataFetch = () => {
     );
   }
   if (error) {
-    return <div>error</div>;
+    return <div>Error: {error}</div>;
   }
 
   return (
-    <div>
+    <div className=" flex flex-col  justify-center items-center gap-5 p-2 md:grid-cols-2 md:grid md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4 lg:container lg:max-w-4xl lg:mx-auto ">
       {venues.map((place) => {
-        return <Card key={place.id} place={place} />;
+        return (
+          <div className="container mx-auto">
+            <Card key={place.id} place={place} />
+          </div>
+        );
       })}
     </div>
   );
