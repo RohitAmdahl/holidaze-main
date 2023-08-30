a50641ff360f3dc0bf31508978e1b218c6f379e5
Rohit kumar <rohit_designer@outlook.com>
2023-08-30 02:32:48 +0200

    edit page

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useState } from "react";
-import { BsSearch } from "react-icons/bs";
+import React from 'react';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 const VenueSearch = ({ onSearch }) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const handelSearch = (e) => {
     onSearch(e.target.value);
@@ -10,21 +10,26 @@ const VenueSearch = ({ onSearch }) => {
   };
 
   return (
-    <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
-      <form className="w-full max-w-md pb-3 pt-3">
-        <div className="flex items-center border-b-2 border-orange py-2 m-2">
-          <BsSearch />
-          <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-            type="text"
-            name="text"
-            value={search}
-            onChange={handelSearch}
-            placeholder="Search......"
-          />
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="container max-w-4xl mx-auto">
+        <h1 className="text-2xl l">Venues for Rent</h1>
+      </div>
+      <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
+        <form className="w-full max-w-md pb-3 pt-3">
+          <div className="flex items-center border-b-2 border-gray-100 py-2 m-2">
+            <BsSearch />
+            <input
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              name="text"
+              value={search}
+              onChange={handelSearch}
+              placeholder="Search......"
+            />
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
