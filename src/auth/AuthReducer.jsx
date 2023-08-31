8486d2caa33aa0776c1e8e7dac51b8449ea8b051
Rohit kumar <rohit_designer@outlook.com>
2023-08-31 15:07:39 +0200

    function folder created and api urls edits

---
+++ src/auth/AuthReducer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthReducer = () => {
+  return <div>AuthReducer</div>;
+};
+
+export default AuthReducer;
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,6 @@
 // api urls
-export const BASE_URL = "https://api.noroff.dev/api/v1/holidaze/venues";
+export const BASE_URL = 'https://api.noroff.dev/api/v1/holidaze/venues';
+
+// auth 'https://nf-api.onrender.com/api/v1/holidaze/auth/register',
+// auth login user 'https://nf-api.onrender.com/api/v1/holidaze/auth/login',
+// fetch profile  `https://nf-api.onrender.com/api/v1/holidaze/profiles/${name}?_bookings=true&_venues=true`,
