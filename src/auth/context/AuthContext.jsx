15b4da8a904be3f308b283e4f69f86b92660ac1e
Rohit kumar <rohit_designer@outlook.com>
2023-09-02 19:57:28 +0200

    update forms

--- src/auth/context/AuthContext.jsx
+++ src/auth/context/AuthContext.jsx
@@ -1,4 +1,1 @@
-// auth login user 'https://nf-api.onrender.com/api/v1/holidaze/auth/login',
-// auth 'https://nf-api.onrender.com/api/v1/holidaze/auth/register',
-
 import { useReducer, createContext } from 'react';
