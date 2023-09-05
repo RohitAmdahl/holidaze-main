c2b1e7107d636be147ef496e9602d2f883a04e3c
Rohit kumar <rohit_designer@outlook.com>
2023-09-05 03:18:31 +0200

    update routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React, { Suspense, lazy } from "react";
-import Layout from "./Layout/index";
-import { Route, Routes } from "react-router-dom";
+import React, { Suspense, lazy, useContext } from 'react';
+import Layout from './Layout/index';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AuthContext } from './auth/Context';
 
-const Homepage = lazy(() => import("./pages/home/HomePage"));
-const DetailPage = lazy(() => import("./pages/specific/DetailPage"));
-const SignIn = lazy(() => import("./pages/signIn/SignIn"));
-const SignUp = lazy(() => import("./pages/signUp/SignUp"));
-const Venues = lazy(() => import("./pages/venues/Venues"));
-const UserProfile = lazy(() => import("./pages/userProfile/Profile"));
-const PageNotFound = lazy(() => import("./pages/pageNotfound/ErrorPage"));
+const Homepage = lazy(() => import('./pages/home/HomePage'));
+const DetailPage = lazy(() => import('./pages/specific/DetailPage'));
+const SignIn = lazy(() => import('./pages/signIn/SignIn'));
+const SignUp = lazy(() => import('./pages/signUp/SignUp'));
+const Venues = lazy(() => import('./pages/venues/Venues'));
+const UserProfile = lazy(() => import('./pages/userProfile/Profile'));
+const PageNotFound = lazy(() => import('./pages/pageNotfound/ErrorPage'));
 
 function App() {
+  const { state } = useContext(AuthContext);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
@@ -18,9 +20,23 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/venues" element={<Venues />} />
           <Route path="/venues/:id" element={<DetailPage />} />
-          <Route path="/signIn" element={<SignIn />} />
+          <Route
+            path="/signIn"
+            element={
+              state.isAuthenticated ? <Navigate to="/profile" /> : <SignIn />
+            }
+          />
           <Route path="/signUp" element={<SignUp />} />
-          <Route path="/profile" element={<UserProfile />} />
+          <Route
+            path="/profile"
+            element={
+              state.isAuthenticated ? (
+                <UserProfile />
+              ) : (
+                <Navigate to="/signIn" />
+              )
+            }
+          />
           <Route path="/error" element={<PageNotFound />} />
         </Route>
       </Routes>
