386c3dc45414b4bcb3e6a1d5ffea5d55f9f1cd44
Rohit kumar <rohit_designer@outlook.com>
2023-09-09 02:51:31 +0200

    file moved

---
+++ src/pages/userProfile/SingleProfile.jsx
@@ -0,0 +1,31 @@
+import { getActionTypes } from '../../auth/context/action';
+const actionTypes = getActionTypes();
+import { BASE_URL } from '../../constants/api';
+export const SingleProfile = async (name) => {
+  const username = localStorage.getItem('userName');
+  console.log(username);
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await fetch(
+      `${BASE_URL}/profiles/${username}?_bookings=true&_venues=true`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error('single profile fetch failed');
+    } else {
+      console.log('response single profile', response);
+    }
+    const singleData = await response.json();
+    console.log('user single profile', singleData);
+    dispatch({ type: actionTypes.PROFILE, payload: singleData });
+  } catch (error) {
+    console.error('User profile Error:', error.message);
+    dispatch({ type: 'error', payload: error.message });
+  }
+};
