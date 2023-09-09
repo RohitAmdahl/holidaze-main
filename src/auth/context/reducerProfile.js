9de2a9128930e1f487da7b04617038f778cb8658
Rohit kumar <rohit_designer@outlook.com>
2023-09-09 02:50:40 +0200

    edit code

---
+++ src/auth/context/reducerProfile.js
@@ -0,0 +1,60 @@
+import { getActionTypes } from './action';
+const actionTypes = getActionTypes();
+export function reducer(state, action) {
+  switch (action.type) {
+    case actionTypes.USER_REGISTER:
+      return {
+        ...state,
+        data: action.payload,
+        error: null,
+      };
+    case actionTypes.LOGIN:
+      const userLogin = action.payload;
+      localStorage.setItem('username', userLogin.profile);
+      localStorage.setItem('accessToken', userLogin.accessToken);
+      localStorage.setItem('avatar', userLogin.avatar);
+      localStorage.setItem('email', userLogin.email);
+      localStorage.setItem('venueManager', userLogin.venueManager);
+
+      return {
+        ...state,
+        isAuthenticated: true,
+        error: null,
+        user: userLogin.profile,
+        // data: action.payload.profile,
+      };
+
+    case actionTypes.USER_LOGOUT: {
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        error: null,
+        user: null,
+      };
+    }
+    case actionTypes.PROFILE: {
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.profile,
+      };
+    }
+    case actionTypes.AVATAR: {
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.profile,
+        isLoading: true,
+      };
+    }
+    case 'error': {
+      return {
+        ...state,
+        error: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+}
