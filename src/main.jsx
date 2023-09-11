f7645b10e660a2e534f60e659c8bb3abad5e41b5
Rohit kumar <rohit_designer@outlook.com>
2023-09-11 14:39:50 +0200

    edit code with css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './auth/context/Context.jsx';
 
