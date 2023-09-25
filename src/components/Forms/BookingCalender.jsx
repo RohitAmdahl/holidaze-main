b1585597f0b879035a7e8af2b575472ed62d6bb7
Rohit kumar <rohit_designer@outlook.com>
2023-09-25 19:26:39 +0200

    calender update

--- src/components/Forms/BookingCalender.jsx
+++ src/components/Forms/BookingCalender.jsx
@@ -1,37 +1,14 @@
-import React, { useState } from 'react';
-
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import 'react-date-range/dist/styles.css'; // additional styles
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { addDays } from 'date-fns';
+import { useState } from 'react';
 
-const BookingCalender = ({ onDatesSelected, booking }) => {
+const BookingCalender = ({ onDatesSelected }) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
-
-  const minSelectableDate = new Date();
-
-  const generateDisabledDates = () => {
-    const dates = [];
-    console.log(dates);
-    booking?.forEach((bookings) => {
-      const startDate = new Date(bookings.dateFrom);
-      const endDate = new Date(bookings.dateTo);
-      while (startDate <= endDate) {
-        dates.push(new Date(startDate));
-        startDate.setDate(startDate.getDate() + 1);
-      }
-    });
-    console.log(booking);
-    console.log('Generated disabledDates:', dates);
-    return dates;
-  };
-
-  const disabledDates = generateDisabledDates();
-
-  console.log('generateDisabledDates:', generateDisabledDates);
 
   const handleDateChange = (dates) => {
     const [start, end] = dates;
@@ -41,45 +18,7 @@ const BookingCalender = ({ onDatesSelected, booking }) => {
     onDatesSelected({ startDate: start, endDate: end });
   };
 
-  const isDateBlocked = (date, startDate, endDate) => {
-    return disabledDates.some((blockedDate) =>
-      isDay(date, blockedDate, startDate, endDate)
-    );
-  };
-
-  console.log(' isDateBlocked', isDateBlocked);
-
-  const isDay = (date, blockedDate, startDate, endDate) => {
-    return (
-      blockedDate.getFullYear() === date.getFullYear() &&
-      blockedDate.getMonth() === date.getMonth() &&
-      blockedDate.getDate() === date.getDate() &&
-      date >= startDate &&
-      date <= endDate
-    );
-  };
-
-  console.log('isDay', isDay);
-  //  statements
-  console.log('disabledDates:', disabledDates);
-  console.log('startDate:', startDate);
-
-  const renderCustomDayContents = (date) => {
-    const isDisabled = isDateBlocked(date, startDate, endDate);
-    const customClassName = isDisabled ? 'custom-disabled-day' : '';
-
-    return (
-      <div className={`custom-day ${customClassName}`}>
-        <span className={isDisabled ? 'disabled-date' : ''}>
-          {date.getDate()}
-        </span>
-      </div>
-    );
-  };
-
-  const getDayClassName = (date) => {
-    return renderCustomDayContents(date, startDate, endDate) ? 'excluded' : '';
-  };
+  const minSelectableDate = new Date();
 
   return (
     <DatePicker
@@ -88,12 +27,10 @@ const BookingCalender = ({ onDatesSelected, booking }) => {
       startDate={startDate}
       endDate={endDate}
       minDate={minSelectableDate}
-      excludeDates={disabledDates}
+      excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
       selectsRange
       selectsStart
       inline
-      renderDayContents={renderCustomDayContents}
-      dayClassName={getDayClassName}
     />
   );
 };
