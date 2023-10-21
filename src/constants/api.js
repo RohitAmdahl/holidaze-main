b50ae3cbff33b110a15cdf4572c3579219c9e99a
rohitAmdahl <rohit_designer@outlook.com>
2023-10-21 20:45:39 +0200

    EMERGENCY:base url updated due to cors error when authentiating user

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,6 +2,8 @@
 export const BASE_URL = 'https://api.noroff.dev/api/v1/holidaze';
 
 export const REGISTER_USER =
-  'https://nf-api.onrender.com/api/v1/holidaze/auth/register';
+  // 'https://nf-api.onrender.com/api/v1/holidaze/auth/register';
+  BASE_URL + '/auth/register';
 export const LOGIN_USER =
-  'https://nf-api.onrender.com/api/v1/holidaze/auth/login';
+  // 'https://nf-api.onrender.com/api/v1/holidaze/auth/login';
+  BASE_URL + '/auth/login';
